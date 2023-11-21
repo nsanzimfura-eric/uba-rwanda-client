2b6a4f665cc957a14642c4cb84336723e25b2db1
nsanzimfura-eric <erickykress1@gmail.com>
2023-11-21 11:43:55 +0200

    create custom snipets and add app gloabl colors

---
+++ src/utils/colors.ts
@@ -0,0 +1,43 @@
+// Colors
+const GLASS = '#1A094A';
+const LIGHT = '#FFFFFF';
+const PALE_LIGHT = 'rgba(255, 255, 255, 0.5)';
+const OPAQUE_LIGHT = 'rgba(255, 255, 255, 0.1)';
+const DARK = '#0C0F14';
+const PALE_DARK = 'rgba(0, 0, 0, 0.5)';
+const ORANGE = '#D17842';
+const GRAY = '#AEAEAE';
+const DARK_GRAY = '#52555A';
+const PINK = 'rgba(170, 126, 182, 0.83)';
+const DARK_PINK = 'rgba(151, 100, 179, 0.3286)';
+const SUCCESS = '#0AA00A';
+const WARNING = '#BCB500';
+const ERROR = '#DC3535';
+
+//BTNS GRADIENTS COLORS
+
+const BRN_GLASS_FILL = 'rgba(209, 120, 66, 0.08)';
+
+// blur effect
+const BLUR = 40;
+
+const colors = {
+  GLASS,
+  LIGHT,
+  PALE_LIGHT,
+  OPAQUE_LIGHT,
+  DARK,
+  PALE_DARK,
+  ORANGE,
+  GRAY,
+  DARK_GRAY,
+  PINK,
+  DARK_PINK,
+  BRN_GLASS_FILL,
+  BLUR,
+  ERROR,
+  SUCCESS,
+  WARNING,
+};
+
+export default colors;
